2a6585f463289f5472ecdfe23be8598faf077c43
Brad Taylor <btaylor@litl.com>
2011-03-01 17:58:53 -0500

    Add timeline app

--- timeline/models.py
+++ timeline/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Event:
+    date = models.DateTimeField(auto_now_add=True, db_index=True)
+    plugin = models.CharField(max_length=32)
+    protagonist = models.ForeignKey(User, related_name='protagonist',
+                                    null=True, db_index=True)
+    deuteragonist = models.ForeignKey(User, related_name='deuteragonist',
+                                      null=True, db_index=True)
+    message = models.CharField()
