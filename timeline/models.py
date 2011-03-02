dcc3f5331f312c0137098dd3ed3b861607adc058
Brad Taylor <brad@getcoded.net>
2011-03-01 23:02:29 -0500

    Add comment field and rename plugin to source

--- timeline/models.py
+++ timeline/models.py
@@ -25,9 +25,10 @@ from django.db import models
 
 class Event:
     date = models.DateTimeField(auto_now_add=True, db_index=True)
-    plugin = models.CharField(max_length=32)
+    source = models.CharField(max_length=32)
     protagonist = models.ForeignKey(User, related_name='protagonist',
                                     null=True, db_index=True)
     deuteragonist = models.ForeignKey(User, related_name='deuteragonist',
                                       null=True, db_index=True)
     message = models.CharField()
+    comment = models.CharField()
