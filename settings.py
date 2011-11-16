ef264df38c547e580242cc2ba1958c8612f19d93
Brad Taylor <btaylor@litl.com>
2011-11-16 14:20:54 -0500

    Modernize database definitions

--- settings.py
+++ settings.py
@@ -12,14 +12,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
-DATABASE_STORAGE_ENGINE = 'INNODB'
+DATABSES = {}
 
 # How far to look back when notifying users about not updating their hours.
 # Set this to a number > 2 if you don't want users going crazy, or worse yet,
